f759decb90a46911b9c3fe750844535ef49e6085
liwenkai <2020583117@qq.com>
2023-10-03 20:51:23 +0800

    Server creation API

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -2,6 +2,8 @@
 
 import { UploadDropzone } from '@/lib/uploadthing'
 import '@uploadthing/react/styles.css'
+import { X } from 'lucide-react'
+import Image from 'next/image'
 
 interface FileUploadProps {
   endpoint: 'messageFile' | 'serverImage'
@@ -10,6 +12,23 @@ interface FileUploadProps {
 }
 
 export const FileUpload: React.FC<FileUploadProps> = ({ endpoint, value, onChange }) => {
+  const fileType = value?.split('.').pop()
+
+  if (value && fileType !== 'pdf') {
+    return (
+      <div className="relative h-20 w-20">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange('')}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    )
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import axios from 'axios'
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
@@ -16,6 +17,7 @@ import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { useEffect, useState } from 'react'
 import { FileUpload } from '../file-upload'
+import { useRouter } from 'next/navigation'
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -28,6 +30,9 @@ const formSchema = z.object({
 
 export const InitialModal = () => {
   const [isMounted, setIsMounted] = useState(false)
+
+  const router = useRouter()
+
   useEffect(() => {
     setIsMounted(true)
   }, [])
@@ -43,7 +48,15 @@ export const InitialModal = () => {
   const isLoading = form.formState.isSubmitting
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+    try {
+      await axios.post('/api/servers', values)
+
+      form.reset()
+      router.refresh()
+      window.location.reload()
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   if (!isMounted) {
